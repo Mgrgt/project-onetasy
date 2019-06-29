ee32eb9ff501e05f84701ada6e29256cbbb6fbc1
Marat Gajsin <52349239+Mgrgt@users.noreply.github.com>
2019-06-29 22:08:57 +0500

    Add files via upload

---
+++ Onetasy/DCS/RandomGen.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+
+namespace DCS
+{
+    class RandomGen
+    {//my random generator
+        public static byte RNCryp(byte Param1)
+        {
+            byte[] MyRN = new byte[1];
+
+            MyRN[0] = Param1++;
+
+            do
+            {
+                using (var rng = new RNGCryptoServiceProvider())
+                {
+                    rng.GetBytes(MyRN);
+                }
+            }
+            while (MyRN[0] >= Param1);
+
+            return (MyRN[0]);
+        }
+    }
+}
