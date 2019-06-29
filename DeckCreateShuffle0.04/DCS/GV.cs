ecdc899d4c34f38a02843b9f72b93d218cea8848
Gajsin Marat <52349239+Mgrgt@users.noreply.github.com>
2019-06-29 19:10:06 +0500

    Add files via upload

---
+++ DeckCreateShuffle0.04/DCS/GV.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCS004
+{
+    //global vars
+    class GV
+    {
+        //the array with possible values
+        private static readonly char[] faces = new char[] { 'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2' };
+        // the array with possible suits
+        private static readonly char[] suits = new char[] { 'c', 'd', 's', 'h' };
+
+        public static char GetFaces(byte Param1)
+        {
+            return faces[Param1];
+        }
+
+        public static char GetSuits(byte Param1)
+        {
+            return suits[Param1];
+        }
+    }
+}
---
+++ DeckCreateShuffle0.04/DCS/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+
+namespace DCS004
+{
+    class Program
+    {
+        static void Main()
+        {
+            WriteLine("Deck 1");
+            //first deck for the banker
+            Deck Deck1 = new Deck();
+            Deck1.Unbox();
+            Deck1.Shuffle();
+            Deck1.Show();
+
+            WriteLine();
+            WriteLine();
+            //second deck for the punter
+            WriteLine("Deck 2");
+            Deck Deck2 = new Deck();
+            Deck2.Unbox();
+            //it's hasn't shuffle
+            Deck2.Show();
+            ReadLine();
+        }
+    }
+}
